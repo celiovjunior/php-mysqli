bae6f14c36ac65aefd317ba3d5a8bbd24b209990
Célio V. Júnior <celio_cjr@hotmail.com>
2023-06-02 21:57:15 +1200

    feat: action CREATE and LIST, config table style

--- all-users.php
+++ all-users.php
@@ -1,1 +1,30 @@
 <h1>all users</h1>
+
+<?php 
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+$qtd = $result->num_rows;
+
+if ($qtd > 0) {
+    print "<table class='table table-hover table-striped'>";
+    print "<tr>";
+    print "<th>#</th>";
+    print "<th>Name</th>";
+    print "<th>Email</th>";
+    print "<th>Date of birth</th>";
+    print "</tr>";
+    
+    while ($row = $result->fetch_object()) {
+        print "<tr>";
+        print "<td>".$row->id."</td>";
+        print "<td>".$row->full_name."</td>";
+        print "<td>".$row->email."</td>";
+        print "<td>".$row->date_of_birth."</td>";
+    }
+    print "</table>";
+} else {
+    print "<p class='alert alert-danger'>Nenhum resultado.</p>";
+}
+
+?>
