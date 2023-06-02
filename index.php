fcfc3fd2855f970e415edf604fd97ec2212dbc28
Célio V. Júnior <celio_cjr@hotmail.com>
2023-06-02 20:24:25 +1200

    feat: built the form page an switch case method for navigation

--- index.php
+++ index.php
@@ -9,7 +9,51 @@
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">System</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=new">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=list">List</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col mt-5">
+                <?php
+                include('./config/database/connection.php');
+                switch (@$_REQUEST["page"]) {
+                    case "new":
+                        include("new-user.php");
+                        break;
+                    case "list":
+                        include("all-users.php");
+                        break;
+                    case "register":
+                        include("register-user.php");
+                        break;
+                    default:
+                        print "Welcome to the system";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 
---
+++ new-user.php
@@ -0,0 +1,22 @@
+<h1>New user:</h1>
+<form action="?page=register" method="POST">
+    <div class="mb-3">
+        <label for="full_name">Full name</label>
+        <input type="text" name="full_name" id="" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="email">E-mail</label>
+        <input type="email" name="email" id="" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="date_of_birth">Date of birth</label>
+        <input type="date" name="date_of_birth" id="" class="form-control">
+    </div>
+    <div class="mb-3">
+        <input type="submit" class="btn btn-primary" value="Register">
+    </div>
+</form>
